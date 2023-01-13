6fb83f7cecab659d35fa56935849e9f9cf11bfc5
spidey-mony <110289642+spidey-mony@users.noreply.github.com>
2023-01-13 21:36:23 +0700

    intialize

---
+++ Triangle.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void Pascal(int n){
+    int arr[n][n];
+    
+    for (int i=0;i<n;i++)
+    {
+    for (int spaces = 0; spaces < (n-i); spaces++)
+    printf(" ");
+
+    for(int j=0;j<=i;j++)
+    {
+        if(i==j || j==0)
+        arr[i][j] = 1;
+    else 
+        arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+        printf("%d",arr[i][j]);
+    }
+    printf("\n");
+}
+}
+int main(){
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d",&n);
+    Pascal(n);
+
+    return 0;
+}
