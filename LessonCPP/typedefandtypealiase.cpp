bbd1a91c482d60853b3015981ad5e0f80c3a5e5d
spidey-mony <110289642+spidey-mony@users.noreply.github.com>
2023-01-14 20:32:28 +0700

    Delete folder

--- LessonCPP/typedefandtypealiase.cpp
+++ LessonCPP/typedefandtypealiase.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 #include<vector>
 
- 
+    // there are typedef and typeallias which means you can define a nickname for data type!
 // typedef std::vector<std::pair<std:: string, int >> pairlist_t;
 // typedef std::string text_t;
 // typedef int number_t;
 //otherwise this is another way that recommended 
-using text_t = std::string;
-using number_t = int;
+   using text_t = std::string;
+   using number_t = int;
 int main(){
     text_t text = "Hello This is Typedef";
     std::cout << text << std::endl;
     number_t number = 19;
     std::cout << number << std::endl;
     return 0;
-}
+} 
